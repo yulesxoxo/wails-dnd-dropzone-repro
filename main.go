320c40d643ebac2b3c250e2779d8fa23726515dd
yulesxoxo <189644144+yulesxoxo@users.noreply.github.com>
2025-10-06 23:51:27 +0200

    repro

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"embed"
+	"log"
+
+	"github.com/wailsapp/wails/v3/pkg/application"
+)
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+func main() {
+
+	app := application.New(application.Options{
+		Name:        "wails-dnd-dropzone-repro",
+		Description: "A demo of using raw HTML & CSS",
+		Assets: application.AssetOptions{
+			Handler: application.AssetFileServerFS(assets),
+		},
+		Mac: application.MacOptions{
+			ApplicationShouldTerminateAfterLastWindowClosed: true,
+		},
+	})
+
+	app.Window.NewWithOptions(application.WebviewWindowOptions{
+		Title:             "Window 1",
+		EnableDragAndDrop: true,
+		Mac: application.MacWindow{
+			InvisibleTitleBarHeight: 50,
+			Backdrop:                application.MacBackdropTranslucent,
+			TitleBar:                application.MacTitleBarHiddenInset,
+		},
+		BackgroundColour: application.NewRGB(27, 38, 54),
+		URL:              "/",
+	})
+
+	err := app.Run()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
